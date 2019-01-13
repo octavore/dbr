1f98f6a1f700d1844300cc1c2c362162ab96b396
tp <tp@octavore.com>
2019-01-13 15:36:29 -0800

    types: Tweak NullTimeField API.

--- types/nulltimefield.go
+++ types/nulltimefield.go
@@ -12,6 +12,6 @@ func NewNullTimeField(table, name string) *NullTimeField {
 	return &NullTimeField{TimeField{Field{table, name}}}
 }
 
-func (f *NullTimeField) As(val *time.Time) (string, *time.Time) {
-	return f.name, val
+func (f *NullTimeField) AsNull() (string, *time.Time) {
+	return f.name, nil
 }
